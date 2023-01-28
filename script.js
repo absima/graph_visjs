35c20606547589ffc90fe28fa1928037a78c9f96
Simachew Mengiste <absima@gmail.com>
2023-01-28 10:41:12 +0100

    first pyScript test

--- script.js
+++ script.js
@@ -1,6 +1,177 @@
 
-import { gpdata } from "./grouping.js"
-
+// import { gpdata } from "./grouping.js"
+const gpdata =
+{
+  "proj1": {
+    "group1": [
+      "Katrin",
+      "Luben",
+      "Jens",
+      "Zan"
+    ],
+    "group2": [
+      "Sima",
+      "Marina",
+      "Julio",
+      "David"
+    ],
+    "group3": [
+      "Fi",
+      "Bruno",
+      "Sivak",
+      "Fabian"
+    ],
+    "group4": [
+      "Victor",
+      "Samin",
+      "Arthi",
+      "Malaiz"
+    ]
+  },
+  "proj2": {
+    "group1": [
+      "Fi",
+      "Sima",
+      "Jens",
+      "Marina"
+    ],
+    "group2": [
+      "Julio",
+      "Luben",
+      "Samin",
+      "Sivak"
+    ],
+    "group3": [
+      "David",
+      "Bruno",
+      "Arthi",
+      "Zan"
+    ],
+    "group4": [
+      "Fabian",
+      "Katrin",
+      "Victor",
+      "Malaiz"
+    ]
+  },
+  "proj3": {
+    "group1": [
+      "David",
+      "Sivak",
+      "Malaiz",
+      "Victor"
+    ],
+    "group2": [
+      "Katrin",
+      "Marina",
+      "Arthi",
+      "Fabian"
+    ],
+    "group3": [
+      "Fi",
+      "Luben",
+      "Jens",
+      "Zan"
+    ],
+    "group4": [
+      "Sima",
+      "Bruno",
+      "Samin",
+      "Julio"
+    ]
+  },
+  "proj4": {
+    "group1": [
+      "Sivak",
+      "Sima",
+      "Fi",
+      "Arthi"
+    ],
+    "group2": [
+      "Malaiz",
+      "Jens",
+      "Fabian"
+    ],
+    "group3": [
+      "David",
+      "Katrin",
+      "Luben",
+      "Zan"
+    ],
+    "group4": [
+      "Bruno",
+      "Samin",
+      "Victor",
+      "Marina"
+    ]
+  },
+  "proj5": {
+    "group1": [
+      "Luben",
+      "David",
+      "Arthi",
+      "Fi"
+    ],
+    "group2": [
+      "Katrin",
+      "Victor",
+      "Fabian",
+      "Sivak"
+    ],
+    "group3": [
+      "Bruno",
+      "Samin",
+      "Marina",
+      "Jens"
+    ]
+  },
+  "proj6": {
+    "group1": [
+      "Sivak",
+      "David",
+      "Katrin",
+      "Samin"
+    ],
+    "group2": [
+      "Victor",
+      "Marina",
+      "Malaiz"
+    ],
+    "group3": [
+      "Fi",
+      "Luben",
+      "Fabian"
+    ],
+    "group4": [
+      "Bruno",
+      "Sima",
+      "Jens"
+    ]
+  },
+  "proj7": {
+    "group1": [
+      "David",
+      "Sima",
+      "Sivak"
+    ],
+    "group2": [
+      "Bruno",
+      "Victor",
+      "Fabian",
+    ],
+    "group3": [
+      "Katrin",
+      "Samin",
+      "Jens",
+      "Fi"
+    ],
+    "group4": [
+      "Marina",
+      "Luben",
+      "Zan"
+    ]
+  }
+}
 
 // create an array with nodes
 // console.log(gpdata.proj1);
@@ -107,12 +278,12 @@ Object.keys(nullremoved).map((name1, iname1) => {
 
 
 // // // // }
-console.log('edges', edges.length);
+console.log('edges', edges);
 // // // console.log(edges)
 const edgez = new vis.DataSet(edges);
 
 // // create a network
-const container = document.getElementById("mynetwork");
+const container = document.getElementById("visual");
 const data = {
   nodes: nodes,
   edges: edgez,
@@ -126,4 +297,6 @@ const options = {
     },
   }
 }
+
+
 let network = new vis.Network(container, data, options);
